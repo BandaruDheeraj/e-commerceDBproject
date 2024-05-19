adad9fc21aceb593c2daab0569c6bb6f8e2df260
BandaruDheeraj <dheeraj.bandaru@hotmail.com>
2024-05-19 09:19:35 -0500

    added queries

--- gui.py
+++ gui.py
@@ -4,16 +4,16 @@ import streamlit as st
 
 
 def main():
-    db = ""
+    db = "ecommerceDB"
 
 
     @st.cache(allow_output_mutation=True)
     def execute_query(database, query):
         # Define your connection details
         conn_details = {
-            'host': 'your_host',
-            'user': 'your_username',
-            'password': 'your_password',
+            'host': 'localhost',
+            'user': 'root',
+            'password': '<M6a0n6h3a7t5t9a1n3>',
             'database': database
         }
 
@@ -48,9 +48,38 @@ def main():
 
     # Define the queries
     queries = {
-        'Query 1': 'SQL query 1',
-        'Query 2': 'SQL query 2',
-        'Query 3': 'SQL query 3',
+        'Retrieve all orders for a customer, including order details, product details, and supplier information, where the order starts from Chicago.': """ SELECT o.Order_ID, o.Date_Placed, o.Status, od.Start_City, od.End_City, p.Description, p.Price, s.Supplier_Name
+        FROM Orders o
+        JOIN Order_Details od ON o.Order_ID = od.Order_ID
+        JOIN Ecommerce ec ON ec.Ecommerce_ID = o.Ecommerce_ID
+        JOIN Inventory i ON ec.Ecommerce_ID = i.Ecommerce_ID
+        JOIN Stores st ON i.Inventory_ID = st.Inventory_ID
+        JOIN Product p ON st.Product_ID = p.Product_ID
+        JOIN Supplier s ON p.Supplier_ID = s.Supplier_ID
+        WHERE od.Start_City = 'Chicago'
+        ORDER BY o.Date_Placed DESC
+        LIMIT {}; """,
+
+        'Query 2': """ SELECT p.Product_ID, p.Description, p.Number_Photos, p.Price, COUNT(od.Order_ID) AS Total_Orders, AVG(p.Price) AS Avg_Price
+        FROM Product p
+        JOIN Supplier s ON p.Supplier_ID = s.Supplier_ID
+        LEFT JOIN Stores st ON p.Product_ID = st.Product_ID
+        LEFT JOIN Inventory i ON st.Inventory_ID = i.Inventory_ID
+        LEFT JOIN Ecommerce ec ON i.Ecommerce_ID = ec.Ecommerce_ID
+        LEFT JOIN Orders o ON ec.Ecommerce_ID = o.Ecommerce_ID
+        LEFT JOIN Order_Details od ON o.Order_ID = od.Order_ID
+        WHERE s.Supplier_ID = 486975277
+        GROUP BY p.Product_ID, p.Description, p.Number_Photos, p.Price
+        ORDER BY Total_Orders DESC; """,
+
+        'Query 3': """ SELECT i.Inventory_ID, p.Product_ID, p.Description, p.Price, i.Quantity, COUNT(p.Product_ID) AS Total_Products
+        FROM Inventory i
+        JOIN Stores st ON i.Inventory_ID = st.Inventory_ID
+        JOIN Product p ON st.Product_ID = p.Product_ID
+        WHERE i.Ecommerce_ID = 605775548
+        GROUP BY i.Inventory_ID, p.Product_ID, p.Description, p.Price, i.Quantity
+        ORDER BY Total_Products DESC
+        LIMIT 10;""",
     }
 
     # Display the radio buttons
